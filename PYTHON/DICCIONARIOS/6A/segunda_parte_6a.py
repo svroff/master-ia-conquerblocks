c62c19e0aaa44a01d739f2bd7c69e8d28ea67af9
Sergi Clinic <Administrador@HCP14287.csc.es>
2026-02-03 18:17:39 +0100

    Finalizada la hoja 6 de ejercicios de diccionarios

--- PYTHON/DICCIONARIOS/6A/segunda_parte_6a.py
+++ PYTHON/DICCIONARIOS/6A/segunda_parte_6a.py
@@ -27,6 +27,7 @@ estudiantes.pop("segundo_alumno")
 estudiantes.get("primer_alumno")["edad"] = 34
 
 print(estudiantes)
+print("------------------------------------------------------")
 
 # --- ANIDAMIENTO DE DICCIONARIOS ---
 
@@ -49,6 +50,8 @@ for clave, valor in productos.items():
     precio_producto = valor["precio"]
     
     print(f"El producto {nombre_producto} cuesta {precio_producto} euros")
+    
+print("------------------------------------------------------")
 
 # 23. Agrega un nuevo producto al diccionario "productos" utilizando una nueva clave y valor.
 productos["tercer_producto"] = {
@@ -61,6 +64,7 @@ for clave, valor in productos.items():
     precio_producto = valor["precio"]
     
     print(f"El producto {nombre_producto} cuesta {precio_producto} euros")
+print("------------------------------------------------------")
 
 # 24. Crea un diccionario llamado "equipos" que contenga tres entradas.
 # Cada entrada representa un equipo deportivo y tiene las claves "nombre" y "jugadores" con sus respectivos valores.
@@ -87,8 +91,31 @@ for clave, valor in equipos.items():
     
     print(f"El equipo {nombre_producto} tiene a {precio_producto} ")
 
+print("------------------------------------------------------")
+
 # 25. Agrega un nuevo equipo al diccionario "equipos" utilizando una nueva clave y valor.
 # La lista de jugadores debe contener al menos tres nombres. 
+equipos["entrada4"] = {
+    "nombre" : "SAP",
+    "jugadores" : ["Sara", "Carlota", "Dani"]
+}
+
+
+for clave, valor in equipos.items():
+    nombre_producto = valor["nombre"]
+    precio_producto = valor["jugadores"]
+    
+    print(f"El equipo {nombre_producto} tiene a {precio_producto} ")
+print("------------------------------------------------------")
 
 # 26. Actualiza la lista de jugadores de uno de los equipos existentes en el diccionario "equipos".
 # Agrega un nuevo jugador a la lista. 
+
+equipos.get("entrada4")["jugadores"].append("Ricard")
+
+for clave, valor in equipos.items():
+    nombre_producto = valor["nombre"]
+    precio_producto = valor["jugadores"]
+    
+    print(f"El equipo {nombre_producto} tiene a {precio_producto} ")
+print("------------------------------------------------------")
