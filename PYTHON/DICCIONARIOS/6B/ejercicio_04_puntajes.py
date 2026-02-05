d1f61d75518ae048c62d05b3c9604d9e4ac71d55
Sergi Clinic <Administrador@HCP14287.csc.es>
2026-02-05 19:05:28 +0100

    A punto de terminar el ejercicio 4 de la hoja 6B

---
+++ PYTHON/DICCIONARIOS/6B/ejercicio_04_puntajes.py
@@ -0,0 +1,34 @@
+# REGISTRO DE PUNTAJES:
+# Implementa un programa en Python que permita registrar y mantener un
+# seguimiento de los puntajes de un juego.
+
+# El programa debe permitir a los jugadores:
+# 1. Registrar nombres y puntajes (Diccionario simple: { "Nombre": Puntos }).
+# 2. Mostrar el puntaje más alto (max).
+# 3. Mostrar el promedio de puntajes (sum / len).
+# 4. Mostrar la cantidad total de jugadores (len).
+
+# --- TU CÓDIGO EMPIEZA AQUÍ ---
+
+# 1. Crea el diccionario con 3 o 4 jugadores y sus puntuaciones (Hardcoded)
+puntos = {
+    "Juan" : 192,
+    "Claus" : 187,
+    "Lorena" : 199,
+    "Sara" : 176
+}
+
+# 2. Calcula el puntaje más alto (Usa max() sobre los valores)
+max_puntos = max(puntos.values())
+jugador_max_puntos = puntos[max_puntos]
+
+print(f'El puntaje más alto lo tiene  con un total de {max_puntos}')
+
+# 3. Calcula el promedio (Suma total / Cantidad de jugadores)
+suma_total = sum(puntos.values())
+cantidad_jugadores = len(puntos)
+print("-" * 30)
+promedio = suma_total / cantidad_jugadores
+print(f'El promedio es de {promedio}')
+
+# 4. Imprime el reporte completo'''
